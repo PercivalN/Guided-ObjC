bef8c9a4ad4e6cec26258c28e302e37e66ba2312
Percy <percival.ngan@gmail.com>
2020-01-23 05:00:51 -0600

    Versions are shown in the cells

---
+++ iOS9-macOS-Versions-Objc/iOS9-macOS-Versions-Objc/LSIVersionsTableViewController.h
@@ -0,0 +1,20 @@
+//
+//  LSIVersionsTableViewController.h
+//  iOS9-macOS-Versions-Objc
+//
+//  Created by Percy Ngan on 1/23/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+// Forward class declaration (no import statement in this file)
+@class LSIVersionController; // IOU / Placeholder
+
+@interface LSIVersionsTableViewController : UITableViewController
+
+@property LSIVersionController *controller;
+
+@end
+
+
---
+++ iOS9-macOS-Versions-Objc/iOS9-macOS-Versions-Objc/LSIVersionsTableViewController.m
@@ -0,0 +1,58 @@
+//
+//  LSIVersionsTableViewController.m
+//  iOS9-macOS-Versions-Objc
+//
+//  Created by Percy Ngan on 1/23/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "LSIVersionsTableViewController.h"
+#import "LSIVersionController.h"
+#import "LSIVersion.h"
+
+@interface LSIVersionsTableViewController ()
+
+@end
+
+@implementation LSIVersionsTableViewController
+
+// Called when we create a ViewController from a Storyboard
+- (instancetype)initWithCoder:(NSCoder *)coder {
+	self = [super initWithCoder:coder];
+	if (self) {
+		_controller = [[LSIVersionController alloc] init];
+	}
+	return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    return self.controller.versions.count; // Need to refer to self in ObjC 
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"VersionCell" forIndexPath:indexPath];
+
+	// Method call
+//	LSIVersion *version = self.controller.versions
+//	LSIVersion *version = [self.controller.versions objectAtIndex:indexPath.row]; // ESC, type method name to get auto-complete
+	LSIVersion *version = [self.controller.versions objectAtIndex:indexPath.row]; // ]
+
+	cell.textLabel.text = version.name;
+	cell.detailTextLabel.text = version.releaseDate; 
+    
+    return cell;
+}
+
+
+
+@end
